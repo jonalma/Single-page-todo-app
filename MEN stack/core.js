61de12da075d9b519e4dec73b02ddc754176bd39
jonucode <jonalmazan@ucode.com>
2015-05-29 21:44:53 +0000

    initial commit

---
+++ MEN stack/core.js
@@ -0,0 +1,10 @@
+/**
+* Created with MEN stack.
+* User: jonucode
+* Date: 2015-05-29
+* Time: 09:18 PM
+* To change this template use Tools | Templates.
+*/
+define(function() {
+return {};
+});
---
+++ server.js
@@ -0,0 +1,42 @@
+// server.js    -------- https://scotch.io/tutorials/creating-a-single-page-todo-app-with-node-and-angular
+// Configure our application
+// Connect to our database
+// Create our Mongoose models
+// Define routes for our RESTful API
+// Define routes for our frontend Angular application
+// Set the app to listen on a port so we can view it in our browser
+
+
+    // set up ========================
+    var express  = require('express');
+    var app      = express();                               // create our app w/ express
+    var mongoose = require('mongoose');                     // mongoose for mongodb
+    var morgan = require('morgan');             // log requests to the console (express4)
+    var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+    var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+    // configuration =================
+
+    mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
+
+    app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+    app.use(morgan('dev'));                                         // log every request to the console
+    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+    app.use(bodyParser.json());                                     // parse application/json
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+    app.use(methodOverride());
+
+
+    // define model ====================
+    var Todo = mongoose.model('Todo', {
+        text : String
+    })
+    
+    
+    // ROUTES =================================================
+    app.get
+    
+
+    // listen (start app with node server.js) ======================================
+    app.listen(8080);
+    console.log("App listening on port 8080");
